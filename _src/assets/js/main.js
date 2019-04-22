8b1a8dffc7ebd891a01b0d1e0d66c5c96855352d
carmen-tm <carmentorremolina@gmail.com>
2019-04-22 16:49:58 +0200

    Search functionality working for results with images

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -7,13 +7,50 @@ const btnEl = document.querySelector('.btn');
 const resultListEl = document.querySelector('.list-results');
 // console.log(inputEl, btnEl, resultListEl);
 
+function createItem(a, b) {
+  return (
+    `<li>
+          <img src="` +
+    b +
+    `" alt=` +
+    a +
+    `>` +
+    `<p>` +
+    a +
+    `</p>` +
+    `</li>`
+  );
+}
+
 //Handler for main button
 function handlerBtn() {
   //Save user input value
-  const userValue = inputEl.nodeValue;
+  const userValue = inputEl.value;
   console.log(userValue);
   //Connect to API
-  // fetch('')
+  fetch(`http://api.tvmaze.com/search/shows?q=${userValue}`)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(responseParsed) {
+      console.log(responseParsed);
+      return responseParsed;
+    })
+    .then(function(data) {
+      //The response is an array of objects
+      const arrShows = data;
+      //   console.log('arrShows', arrShows);
+      for (const element of arrShows) {
+        const arrNames = element.show.name;
+        const arrUrls = element.show.image.medium;
+        console.log(arrNames, arrUrls);
+
+        const itemsFilled = createItem(arrNames, arrUrls);
+        console.log(itemsFilled);
+
+        resultListEl.innerHTML += itemsFilled;
+      }
+    });
 }
 
 //Add lister to main button
