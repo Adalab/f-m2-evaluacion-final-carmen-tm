3e62a093393cc8e719fe3ae978c251dda2477ea5
carmen-tm <carmentorremolina@gmail.com>
2019-04-22 16:13:27 +0200

    Start search functionality

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,2 +1,20 @@
 /* eslint-disable no-console */
 'use strict';
+
+//Get elements
+const inputEl = document.querySelector('.app-input');
+const btnEl = document.querySelector('.btn');
+const resultListEl = document.querySelector('.list-results');
+// console.log(inputEl, btnEl, resultListEl);
+
+//Handler for main button
+function handlerBtn() {
+  //Save user input value
+  const userValue = inputEl.nodeValue;
+  console.log(userValue);
+  //Connect to API
+  // fetch('')
+}
+
+//Add lister to main button
+btnEl.addEventListener('click', handlerBtn);
--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,4 +1,22 @@
 /* eslint-disable no-console */
 'use strict';
 
+//Get elements
+const inputEl = document.querySelector('.app-input');
+const btnEl = document.querySelector('.btn');
+const resultListEl = document.querySelector('.list-results');
+// console.log(inputEl, btnEl, resultListEl);
+
+//Handler for main button
+function handlerBtn() {
+  //Save user input value
+  const userValue = inputEl.nodeValue;
+  console.log(userValue);
+  //Connect to API
+  // fetch('')
+}
+
+//Add lister to main button
+btnEl.addEventListener('click', handlerBtn);
+
 //# sourceMappingURL=main.js.map
