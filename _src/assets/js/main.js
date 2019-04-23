4bd39ad7a94b482b559917dc12635e5ea1e9ce1e
carmen-tm <carmentorremolina@gmail.com>
2019-04-23 13:01:28 +0200

    Remake add results with search btn

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -5,37 +5,23 @@
 const inputEl = document.querySelector('.app-input');
 const btnEl = document.querySelector('.btn');
 
+//Get list elements to fill with items
 const resultListEl = document.querySelector('.results__list');
 const favouritesListEl = document.querySelector('.favourites__list');
 
+//Empty array for items results from search
+let resultsArr = [];
 //Empty array for storing favourites shows
 const myFavShowsArr = [];
 //Emtpy array of objects for storing in LS
 const favArrObjects = [];
 
-function createItem(a, b) {
-  //Create li elements
-  const newItemEl = document.createElement('li');
-  newItemEl.classList.add('show-card');
-
-  //Create content node
-  const contentItemImgEl = document.createElement('img');
-  contentItemImgEl.setAttribute('src', b);
-  contentItemImgEl.setAttribute('alt', a);
-
-  const contentItemTitleEl = document.createElement('h3');
-  const contentItemTitleText = document.createTextNode(a);
-  contentItemTitleEl.appendChild(contentItemTitleText);
-
-  newItemEl.appendChild(contentItemImgEl);
-  newItemEl.appendChild(contentItemTitleEl);
-  //   console.log(newItemEl);
-
-  return newItemEl;
-}
-
-function paintResults(array, list) {
+function createItemsFromSearch(array) {
+  //Reset array
+  resultsArr = [];
+  //Iterate the api results to create items with content
   for (const element of array) {
+    // console.log(array);
     const arrNames = element.show.name;
     let arrUrls = '';
 
@@ -44,183 +30,38 @@ function paintResults(array, list) {
     } else {
       arrUrls = element.show.image.medium;
     }
+    // console.log(arrNames, arrUrls);
 
-    const itemsFilled = createItem(arrNames, arrUrls);
+    //Create li elements
+    const newItemEl = document.createElement('li');
+    newItemEl.classList.add('show-card');
 
-    list.appendChild(itemsFilled);
+    //Create content node
+    const contentItemImgEl = document.createElement('img');
+    contentItemImgEl.setAttribute('src', arrUrls);
+    contentItemImgEl.setAttribute('alt', arrNames);
+
+    const contentItemTitleEl = document.createElement('h3');
+    const contentItemTitleText = document.createTextNode(arrNames);
+    contentItemTitleEl.appendChild(contentItemTitleText);
+
+    //Add id to make easier further instructions
+    newItemEl.setAttribute('id', arrNames);
+
+    newItemEl.appendChild(contentItemImgEl);
+    newItemEl.appendChild(contentItemTitleEl);
+
+    //Fill empty array with all items:
+    resultsArr.push(newItemEl);
   }
+  // console.log('my array of results is working', resultsArr);
+  return resultsArr;
 }
 
-//LS: First, create array of objects
-function createArrayObjects(array) {
-  for (let i = 0; i < array.length; i++) {
-    const favImgEl = array[i].firstElementChild;
-    const favImgUrl = favImgEl.src;
-
-    const favTitleEl = array[i].lastElementChild;
-    const favTitleText = favTitleEl.innerHTML;
-    // console.log(favTitleText);
-
-    favArrObjects[i] = {
-      url: favImgUrl,
-      title: favTitleText
-    };
-    // console.log(favArrObjects[i]);
-  }
-}
-
-//LS: Secondly, store the array in LS
-function storeInLS(key, array) {
-  localStorage.setItem(key, JSON.stringify(array));
-}
-
-//LS: Finally, retrieve info from LS
-function refreshPage() {
-  //Check if LS has something
-  const infoSavedInLS = JSON.parse(localStorage.getItem('favArrObjects'));
-
-  if (infoSavedInLS) {
-    console.log('caché has something', infoSavedInLS);
-
-    for (const object of infoSavedInLS) {
-      const myUrl = object.url;
-      const myTitle = object.title;
-      console.log(myUrl, myTitle);
-
-      //With this info, I can create Items
-      const mySavedFav = createItem(myTitle, myUrl);
-      mySavedFav.classList.add('show-card--favourite');
-      mySavedFav.classList.add('preview--favourite');
-      console.log('mySavedFav', mySavedFav);
-
-      //And paint them in HTML
-      paintResultsReduced(mySavedFav, favouritesListEl);
-
-      //Add reset button
-      addResetBtn(myTitle, mySavedFav);
-    }
-  } else {
-    console.log('caché is empty');
-  }
-}
-
-function addResetBtn(title, item) {
-  //Create btn reset
-  const resetBtnEl = document.createElement('button');
-  resetBtnEl.classList.add('reset-btn');
-  const resetBtnContent = document.createTextNode('x');
-
-  resetBtnEl.appendChild(resetBtnContent);
-  resetBtnEl.setAttribute('data--id', title);
-  item.appendChild(resetBtnEl);
-}
-
-function createFavItem(a) {
-  //Make a copy of the li element as favourite
-  const favItemCloned = a.cloneNode(true);
-  favItemCloned.classList.add('preview--favourite');
-  const title = favItemCloned.innerText;
-
-  //Create an id matching its title
-  favItemCloned.setAttribute('id', title);
-  console.log('favItemCloned', favItemCloned);
-
-  addResetBtn(title, favItemCloned);
-
-  return favItemCloned;
-}
-
-function paintResultsReduced(array, list) {
-  list.appendChild(array);
-}
-
-function selectFavourite(a) {
-  //Include favourite items in an array
-  //NOT WORKING. IF NOT FAVOURITE (NOT HAVING THAT CLASS), IT SHOULD REMOVE IT FROM THE ARRAY
-  if (a.classList.contains('show-card--favourite')) {
-    myFavShowsArr.push(a);
-    console.log('myFavShowsArr', myFavShowsArr);
-    //   } else {
-    //     delete myFavShowsArr[a];
-    //     console.log('myFavShowsArr', myFavShowsArr);
-  }
-}
-
-function handlerResetBtnClick(event) {
-  const resetBtnClicked = event.currentTarget;
-  const resetBtnClickedData = resetBtnClicked.getAttribute('data--id');
-  console.log('click', resetBtnClickedData);
-  // console.dir(resetBtnClicked);
-
-  // const itemForRemove = getElementById(resetBtnClickedData);
-  const itemForRemove = resetBtnClicked.parentNode;
-  console.log(itemForRemove);
-
-  favouritesListEl.removeChild(itemForRemove);
-
-  //To remove the item from my array of favourites I need to find its index
-  for (const element of myFavShowsArr) {
-    const titleFav = element.innerText;
-    const idFavRemove = itemForRemove.getAttribute('id');
-    if (titleFav === idFavRemove) {
-      console.log('quiero borrar este item');
-
-      const myIndex = myFavShowsArr.indexOf(element);
-      console.log(myIndex);
-      //The method splice() remove one element knowing it index. The inputs are the index point to start at and the number of elements to remove.
-      myFavShowsArr.splice(myIndex, 1);
-      console.log(myFavShowsArr);
-
-      ///MISSING JUST REMOVING THE FAV CLASS ON THE LIST OF RESULTS!!!
-    }
-  }
-  // console.log(itemForRemove);
-  // console.log(myFavShowsArr);
-  // console.log(myIndex);
-}
-
-function handlerCardsClick(event) {
-  const selectedCard = event.currentTarget;
-  //   console.log('selecting one card', selectedCard);
-
-  //Add a special class for favourites
-  selectedCard.classList.add('show-card--favourite');
-
-  selectFavourite(selectedCard);
-
-  //Copy items
-  const favItems = createFavItem(selectedCard);
-
-  //Paint favourite results on its list
-  // paintResultsReduced(favItems, favouritesListEl);
-  console.log('favItems', favItems);
-  console.log('myFavShowsArr', myFavShowsArr);
-
-  const resetArrayBtnEl = document.querySelectorAll('.reset-btn');
-  // console.log(resetArrayBtnEl);
-
-  ///////////////
-  // LOCAL STORAGE
-
-  // console.log('array to be stored in LS', myFavShowsArr);
-
-  //initial info: array of favourites
-  // const fakeArray = document.querySelectorAll('.fake-item');
-  // console.log(fakeArray);
-
-  createArrayObjects(myFavShowsArr);
-
-  //I have an array of objects to be store in LS
-  // console.log('favArrObjects', favArrObjects);
-
-  storeInLS('favArrObjects', favArrObjects);
-
-  refreshPage();
-  ////////////////////
-
-  //Add listener to each reset butoton
-  for (const button of resetArrayBtnEl) {
-    button.addEventListener('click', handlerResetBtnClick);
+//Append each li to its list
+function paintResults(array, list) {
+  for (const element of array) {
+    list.appendChild(element);
   }
 }
 
@@ -240,20 +81,23 @@ function handlerBtnSearch() {
     .then(function(data) {
       //The response is an array of objects
       const arrShows = data;
-      //   console.log('arrShows', arrShows);
+      console.log('arrShows', arrShows);
 
-      //Reset results
+      //Reset results list on every search
       resultListEl.innerHTML = '';
-      //Paint results
-      paintResults(arrShows, resultListEl);
 
-      //Add listener to each card from the results
-      const resultsCardEl = document.querySelectorAll('.show-card');
-      for (const card of resultsCardEl) {
-        card.addEventListener('click', handlerCardsClick);
-      }
+      //FIRST Create li items from data
+      createItemsFromSearch(arrShows);
+      //SECOND Paint li results
+      paintResults(resultsArr, resultListEl);
+
+      //Add listener to each card from the results to add Favourites functionality
+      // const resultsCardEl = document.querySelectorAll('.show-card');
+      // for (const card of resultsCardEl) {
+      //   card.addEventListener('click', handlerCardsFavClick);
+      // }
     });
 }
 
-//Add lister to main button
+//Add lister to main Search button
 btnEl.addEventListener('click', handlerBtnSearch);
